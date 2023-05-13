f49a72fdea34b01c7a274139a899475abf8e0f7b
MircoBeltrame <109956639+MircoBeltrame@users.noreply.github.com>
2023-05-13 14:36:55 +0200

    Update time_complexity.py
    File definitvo per creazione grafici

--- time_complexity.py
+++ time_complexity.py
@@ -1,58 +1,11 @@
 from timeit import default_timer as timer
 import matplotlib.pyplot as plt
 
-#attack time complexity vs M
-#fix a k and make u bigger
-
-n_iter = 100
+n_iter = 200
 max_length = 400
-
-k ='010101010'
-Times = []
-for j in range(n_iter):
-  times = []
-  for i in range(max_length):
-    u = '10'*(i+1)
-    start = timer()
-    substitution_attack(u)
-    end = timer()
-    times.append(end-start)
-  Times.append(times)
-avg = []
-
-for i in range(max_length):
-  avg.append(sum(t[i] for t in Times)/len(Times))
-
-plt.plot(range(len(avg)),avg)
-plt.title('attack time complexity vs M')
-plt.xlabel('length [bits]')
-plt.ylabel('time [s]')
-
-#attack time complexity vs K
-#fix a u and make k bigger
-
-u ='010101010'
-Times = []
-for j in range(n_iter):
-  times = []
-  for i in range(max_length):
-    k = '10'*(i+1)
-    start = timer()
-    substitution_attack(u)
-    end = timer()
-    times.append(end-start)
-  Times.append(times)
-avg = []
-for i in range(max_length):
-  avg.append(sum(t[i] for t in Times)/len(Times))
-
-plt.plot(range(len(avg)),avg)
-plt.title('attack time complexity vs K')
-plt.xlabel('length [bits]')
-plt.ylabel('time [s]')
-
 #signing time complexity vs M
 #fix a k and make u bigger
+
 k ='010101010'
 Times = []
 for j in range(n_iter):
@@ -69,9 +22,6 @@ for i in range(max_length):
   avg.append(sum(t[i] for t in Times)/len(Times))
 
 plt.plot(range(len(avg)),avg)
-plt.title('signing time complexity vs M')
-plt.xlabel('length [bits]')
-plt.ylabel('time [s]')
 
 #signing time complexity vs K
 #fix a u and make k bigger
@@ -91,12 +41,14 @@ for i in range(max_length):
   avg.append(sum(t[i] for t in Times)/len(Times))
 
 plt.plot(range(len(avg)),avg)
-plt.title('signing time complexity vs K')
+plt.title('signing time complexity')
 plt.xlabel('length [bits]')
 plt.ylabel('time [s]')
-
+plt.legend(['vs M','vs K'])
+plt.show()
 #veryfying time complexity vs M
 #fix a k and make u bigger
+
 k ='010101010'
 Times = []
 for j in range(n_iter):
@@ -114,9 +66,7 @@ for i in range(max_length):
   avg.append(sum(t[i] for t in Times)/len(Times))
 
 plt.plot(range(len(avg)),avg)
-plt.title('veryfying time complexity vs M')
-plt.xlabel('length [bits]')
-plt.ylabel('time [s]')
+
 
 #veryfying time complexity vs K
 #fix a u and make k bigger
@@ -137,6 +87,54 @@ for i in range(max_length):
   avg.append(sum(t[i] for t in Times)/len(Times))
 
 plt.plot(range(len(avg)),avg)
-plt.title('veryfying time complexity vs K')
+plt.title('verifying time complexity')
 plt.xlabel('length [bits]')
 plt.ylabel('time [s]')
+plt.legend(['vs M','vs K'])
+plt.show()
+
+#attack time complexity vs M
+#fix a k and make u bigger
+
+k ='010101010'
+Times = []
+for j in range(n_iter):
+  times = []
+  for i in range(max_length):
+    u = '10'*(i+1)
+    start = timer()
+    substitution_attack(u)
+    end = timer()
+    times.append(end-start)
+  Times.append(times)
+avg = []
+
+for i in range(max_length):
+  avg.append(sum(t[i] for t in Times)/len(Times))
+
+plt.plot(range(len(avg)),avg)
+
+#attack time complexity vs K
+#fix a u and make k bigger
+
+u ='010101010'
+Times = []
+for j in range(n_iter):
+  times = []
+  for i in range(max_length):
+    k = '10'*(i+1)
+    start = timer()
+    substitution_attack(u)
+    end = timer()
+    times.append(end-start)
+  Times.append(times)
+avg = []
+for i in range(max_length):
+  avg.append(sum(t[i] for t in Times)/len(Times))
+
+plt.plot(range(len(avg)),avg)
+plt.title('attack time complexity')
+plt.xlabel('length [bits]')
+plt.ylabel('time [s]')
+plt.legend(['vs M','vs K'])
+plt.show()
